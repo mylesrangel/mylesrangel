e9ac823a406e09829c5e76d308f420263a82fea5
myles <myles.rangel@gmail.com>
2019-02-06 00:30:14 -0800

    Added first Clas

---
+++ mylesrangel/src/Components/homePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+
+class Homepage extends Component{
+	
+
+	render(){
+		return(
+			<div>
+				<p> Test </p>
+			</div>
+		);
+	}
+}
+
+export default Homepage;
