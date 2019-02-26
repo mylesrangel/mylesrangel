c3d60ca96159511989df2a2d315a0c1bfedcbaca
myles <myles.rangel@gmail.com>
2019-02-26 15:07:08 -0800

    Added content to contact form

--- mylesrangel/src/Components/Contact/contact.js
+++ mylesrangel/src/Components/Contact/contact.js
@@ -10,8 +10,27 @@ const Contact = () => {
 
 
 	return(
-			<div id="contactForm">
-				<p> Test Test Test Contact </p>
+			<div >
+				<form id="contactForm" >
+					<p className="formElements">
+						<label> First Name: </label>
+						<input id="firstName" placeholder="Your first name" />
+					</p>
+					<p className="formElements">
+						<label> Last Name: </label>
+						<input id="lastName" placeholder="Your last name" />
+					</p>
+					<p className="formElements">
+						<label> Email: </label>
+						<input id="email" placeholder="What is your email?" />
+					</p>
+					<p className="formElements">
+						<label> Message </label>
+						<br />
+						<textarea id="message" placeholder="What would you like to tell me?"> </textarea>
+					</p>
+						<input type="submit" value="submit" />  
+				</form>
 			</div>
 		)
 
