1302d7c4cda5f48201664fc1b6b977c7b76711c8
Rangel <icheckmyownmail@gmail.com>
2019-04-17 00:20:43 -0700

    Nodemailer is working and talking with client info

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 
 const app = express();
 
@@ -18,6 +19,45 @@ app.get('/', (req, res) =>{
 
 app.post('/sendemail', (req, res) =>{
     console.log(req.body);
+
+    const transporter = nodemailer.createTransport({
+        //service: 'smtp.stackmail.com',
+        host: 'smtp.stackmail.com',
+        secure: true,
+        port: '465',
+        auth:{
+            user: 'websitecontact@mylesrangel.com',
+            pass: 'Ce2e79a9b'
+        },
+        tls:{
+            rejectUnauthorized: false
+        }
+    });
+    const mailOptions = {
+        from: 'websitecontact@mylesrangel.com',
+        to: 'myles@mylesrangel.com',
+        firstName: `${req.body.firstName}`,
+        lastName: `${req.body.lastName}`,
+        subject: `${req.body.subject}`,
+        message: `${req.body.message}`,
+        replyto: `${req.body.email}`
+    };
+
+    // transporter.verify(function(error,success){
+    //     if(error){
+    //         console.log(error);
+    //     }else{
+    //         console.log("Server is ready for greatness...");
+    //     }
+    // });
+
+    transporter.sendMail(mailOptions, function (err, res){
+        if(err){
+            console.error('there was an error: ', err);
+        }else{
+            console.log('here is the res: ', res);
+        }
+    });
     // console.log(req.body.firstName);
     // console.log(req.body.lastName);
     // console.log(req.body.email);
