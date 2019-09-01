61e7d50680aaf157728abac45aa930e6dfc964c0
Myles <myles.rangel@gmail.com>
2019-08-31 23:22:06 -0700

    Added cropped image for phone and added phone layout(under 500px)

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -4,36 +4,79 @@ import MediaQuery from 'react-responsive';
 
 import { Icon , Button } from 'semantic-ui-react'
 
+
 import Logo from '../../Media/logo.png';
 import BackgroundImage from '../../Media/laptop-image-full-1900.png';
-
-
+import BackgroundImageTablet from '../../Media/phone-image-full-edited.png';
+import BackgroundImagePhone from '../../Media/phone-image-full-edited-cropped.png';
+import myResume from "../../Media/MylesRangel.pdf";
 
 import "./about.css";
 
 class About extends Component{
 	render(){
 		return(
-			<div id="homepageContainer">
-                <img id = "logoImage" src = {Logo} alt = "laptop" />
-				<img id= "backgroundImage" src = {BackgroundImage} alt = "laptop" />
-				<p id = "myStatement">Hello, I'm Myles, I am a full Stack Developer! I have been developing full time since 2017. Feel free to have a look around. </p>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/">
-                    <Button circular basic inverted id='githubAbout' icon = "github" size='massive' />
-                </a>
-                <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mylesrangel/">
-                    <Button circular basic color="blue" id='linkedinAbout' icon = "linkedin" size='massive' />
-                </a>
-                <Link to="/">
-                    <Button circular id='homeIcon' icon= "home" size='big' />
-				</Link>
-			</div>
+            <div>
+                <MediaQuery maxWidth = {2800}>
+                    <div id="homepageContainer">
+                        <img id = "logoImage" src = {Logo} alt = "laptop" />
+                        <img id= "backgroundImage" src = {BackgroundImage} alt = "laptop" />
+                        <p id = "myStatement">Hello, I'm Myles, I am a full Stack Developer! I have been developing full time since 2017. Feel free to have a look around. </p>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/">
+                            <Button circular basic inverted id='githubAbout' icon = "github" size='massive' />
+                        </a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mylesrangel/">
+                            <Button circular basic color="blue" id='linkedinAbout' icon = "linkedin" size='massive' />
+                        </a>
+                        <Link to="/">
+                            <Button circular id='homeIcon' icon= "home" size='big' />
+                        </Link>
+                    </div>
+                </MediaQuery>
+                
+                {/* ----------------------------Tablet 501 < 799 -------------------------------------------- */}
+
+                <MediaQuery minWidth ={501} maxWidth = {799} >
+                    <div id="homepageContainerTablet">
+                        <img id = "logoImageTablet" src = {Logo} alt = "laptop" />
+                        <img id= "backgroundImageTablet" src = {BackgroundImageTablet} alt = "laptop" />
+                        <p id = "myStatementTablet">Hello, I'm Myles, I am a full Stack Developer! I have been developing full time since 2017. Feel free to have a look around. </p>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/">
+                            <Button circular basic inverted id='githubAbout' icon = "github" size='massive' />
+                        </a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mylesrangel/">
+                            <Button circular basic color="blue" id='linkedinAbout' icon = "linkedin" size='massive' />
+                        </a>
+                        <Link to="/">
+                            <Button circular id='homeIcon' icon= "home" size='big' />
+                        </Link>
+                    </div>                
+                </MediaQuery>
+
+
+
+    {/* ---------------------Mobile 500px -------------------------------------------------------------*/}
+
+                <MediaQuery maxWidth = {500} >
+                <div id="homepageContainerPhone">
+                        <img id = "logoImagePhone" src = {Logo} alt = "laptop" />
+                        <img id= "backgroundImagePhone" src = {BackgroundImagePhone} alt = "laptop" />
+                        <p id = "myStatementPhone">Hello, I'm Myles, I am a full Stack Developer! I have been developing full time since 2017. Feel free to have a look around. </p>
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/">
+                            <Button circular basic inverted id='githubAbout' icon = "github" size='massive' />
+                        </a>
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mylesrangel/">
+                            <Button circular basic color="blue" id='linkedinAbout' icon = "linkedin" size='massive' />
+                        </a>
+                        <Link to="/">
+                            <Button circular id='homeIcon' icon= "home" size='big' />
+                        </Link>
+                    </div>
+                </MediaQuery>
+            </div>
 		);
 	}
 
-	componentWillUnmount() {
-
-    }
 }
 
 export default About;
--- src/Components/Homepage/homePage.js
+++ src/Components/Homepage/homePage.js
@@ -5,7 +5,8 @@ import { Icon , Button } from 'semantic-ui-react'
 
 import Logo from '../../Media/logo.png';
 import BackgroundImage from '../../Media/laptop-image-full-1900.png';
-import BackgroundImagePhone from '../../Media/phone-image-full-edited.png';
+import BackgroundImageTablet from '../../Media/phone-image-full-edited.png';
+import BackgroundImagePhone from '../../Media/phone-image-full-edited-cropped.png';
 import myResume from "../../Media/MylesRangel.pdf";
 
 import {Link} from "react-router-dom";
@@ -76,7 +77,7 @@ hideclockSpecialtyTechStack = () => {
 		return(
 
 			<div>
-				<MediaQuery query = "(min-width: 800px)" >
+				<MediaQuery minWidth = {800} maxWidth = {2800} >
 					<div id="homepageContainer">
 						<img id = "backgroundImage" src = {BackgroundImage} alt = "laptop" />
 						<img id = "logoImage" src = {Logo} alt = "logo" />
@@ -141,8 +142,62 @@ hideclockSpecialtyTechStack = () => {
 
 				{/* -----------------Under 800px--------------------------------------- -------------------------------------------------------------------------------------------------------------------------------*/}
 
-				<MediaQuery query = "(max-width: 799px)" >
-					<div id="homepageContainer">
+				<MediaQuery minWidth ={501} maxWidth = {799} >
+					<div id="homepageContainerTablet">
+						<img id = "backgroundImageTablet" src = {BackgroundImageTablet} alt = "laptop" />
+						<img id = "logoImageTablet" src = {Logo} alt = "logo" />
+						<Button circular id='homeIconTablet' icon= "home" size='large' />
+						<div id = "techIconsTablet" >
+							{this.state.reactTech && <Button circular basic color='blue' id='react' icon = "react" /> }
+							{this.state.jsTech &&<Button circular basic color='green' id='js' icon = "js" />}
+							{this.state.awsTech &&<Button circular basic color='orange' id='aws' icon = "aws" />}
+							{this.state.html5Tech &&<Button circular basic color='orange' id='html5' icon = "html5" />}
+							{this.state.css3Tech &&<Button circular basic color='blue' id='css3' icon = "css3" />}
+							{this.state.phpTech &&<Button circular basic color='purple' id='php' icon = "php"  />}
+							{this.state.npmTech &&<Button circular basic color='blue' id='npm' icon = "npm" />}
+							{this.state.githubTech &&<Button circular basic color='black' id='github' icon = "github" />}
+							{this.state.nodeTech &&<Button circular basic color='green' id='node' icon = "node" />}
+							{this.state.trelloTech &&<Button circular basic color='blue' id='trello' icon = "trello" />}
+							{this.state.androidTech &&<Button circular basic color='green' id='android' icon = "android" />}
+						</div>
+						<div id = "computerScreenContainerTopTablet">
+							<Link to="/about" >
+								<Button circular basic color="red" id='addressCard' icon = "address card"/>
+								<p>About</p>
+							</Link>					
+							<a href = {myResume} target="_blank" rel="noopener noreferrer">
+								<Button circular basic color="blue" id='file outline' icon = "file outline" />
+								<p>Resume</p>
+							</a>
+							<Link to="/contact" >
+								<Button circular basic color= "green" id='idCard' icon = "envelope" />
+								<p>Contact</p>
+							</Link>
+						</div>
+						<div id = "projectSeperatingTablet">
+							<hr id="leftHorizontalTablet" />
+							Projects
+							<hr id="rightHorizontalTablet"/>
+						</div>
+						<div id = "projectLinksTablet">
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/mylesrangel">
+									<Button onMouseEnter = {this.showMylesrangelTechStack}  onMouseLeave = {this.hideMylesrangelTechStack} circular basic color="green"id='addressCard' icon = "file code" />
+									<p>mylesrangel.com</p>
+							</a>
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/ClockSpecialty">
+									<Button onMouseEnter = {this.showclockSpecialtyTechStack}  onMouseLeave = {this.hideclockSpecialtyTechStack}circular basic color="teal" id='addressCard' icon = "file code" />
+									<p>Clock Specialty</p>
+							</a>					
+						</div>
+					</div>
+				</MediaQuery>
+
+
+
+{/* ---------------------Mobile 500px -------------------------------------------------------------*/}
+
+				<MediaQuery maxWidth = {500} >
+					<div id="homepageContainerMobile">
 						<img id = "backgroundImageMobile" src = {BackgroundImagePhone} alt = "laptop" />
 						<img id = "logoImageMobile" src = {Logo} alt = "logo" />
 						<Button circular id='homeIconMobile' icon= "home" size='big' />
@@ -161,15 +216,15 @@ hideclockSpecialtyTechStack = () => {
 						</div>
 						<div id = "computerScreenContainerTopMobile">
 							<Link to="/about" >
-								<Button circular basic color="red" id='addressCard' icon = "address card" />
+								<Button circular basic color="red" id='addressCard' icon = "address card" size = "huge"/>
 								<p>About</p>
 							</Link>					
 							<a href = {myResume} target="_blank" rel="noopener noreferrer">
-								<Button circular basic color="blue" id='file outline' icon = "file outline" />
+								<Button circular basic color="blue" id='file outline' icon = "file outline" size = "huge"/>
 								<p>Resume</p>
 							</a>
 							<Link to="/contact" >
-								<Button circular basic color= "green" id='idCard' icon = "envelope" />
+								<Button circular basic color= "green" id='idCard' icon = "envelope" size = "huge"/>
 								<p>Contact</p>
 							</Link>
 						</div>
@@ -180,16 +235,17 @@ hideclockSpecialtyTechStack = () => {
 						</div>
 						<div id = "projectLinksMobile">
 							<a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/mylesrangel">
-									<Button onMouseEnter = {this.showMylesrangelTechStack}  onMouseLeave = {this.hideMylesrangelTechStack} circular basic color="green"id='addressCard' icon = "file code" />
+									<Button onMouseEnter = {this.showMylesrangelTechStack}  onMouseLeave = {this.hideMylesrangelTechStack} circular basic color="green"id='addressCard' icon = "file code"  size = "big"/>
 									<p>mylesrangel.com</p>
 							</a>
 							<a target="_blank" rel="noopener noreferrer" href="https://github.com/mylesrangel/ClockSpecialty">
-									<Button onMouseEnter = {this.showclockSpecialtyTechStack}  onMouseLeave = {this.hideclockSpecialtyTechStack}circular basic color="teal" id='addressCard' icon = "file code" />
+									<Button onMouseEnter = {this.showclockSpecialtyTechStack}  onMouseLeave = {this.hideclockSpecialtyTechStack}circular basic color="teal" id='addressCard' icon = "file code"  size = "big"/>
 									<p>Clock Specialty</p>
 							</a>					
 						</div>
 					</div>
 				</MediaQuery>
+				
 			</div>
 		);
 	}
